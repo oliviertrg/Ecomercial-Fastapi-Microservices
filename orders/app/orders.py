7ffdee9371aade5ff5cf689f597b9a2e79ed51c8
oliviertrg <rioverrain@gmail.com>
2023-06-02 16:20:05 -0400

    add new feature

--- orders/app/orders.py
+++ orders/app/orders.py
@@ -56,29 +56,27 @@ def create_order(new_order : schema.add,current_user : int = Depends(auth2.get_c
      
  return new_order
 
-@router.delete('/delete-items/{order_id}',status_code=status.HTTP_204_NO_CONTENT)
-async def delete_order(order_id : int,item_id : int,current_user : int = Depends(auth2.get_current_user)):
-   db = curso()
-   c = db.cursor()
-   try: 
-    sql = f"""select * from "cart" where "orders_id" = '{order_id}' ; """
+@router.delete('/delete-items/{item_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_order(item_id : int,current_user : int = Depends(auth2.get_current_user)):
+    db = curso()
+    c = db.cursor()
+    sql = f"""select * from "cart" where id_customer = '{int(current_user.id)}' and orders_status = 'unpaid' ; """
     c.execute(sql)
     x = c.fetchall()
+    print(x)
     if len(x) == 0 :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"order with id: {order_id} does not exist")
-    if x[0][1] != int(current_user.id) :
+                            detail=f"order with users {int(current_user.id)} does not exist")
+    elif x[0][0] != int(current_user.id) :
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Not authorized to perform requested action")
     else:
-        sql1 = f"""delete from "cart" where "orders_id" = '{order_id}'  
-                                      and "item_id" = '{item_id}' ;"""
+        sql1 = f"""delete from "cart" where id_customer = '{int(current_user.id)}'
+                                      and orders_status = 'unpaid'  
+                                      and "item_id" = '{item_id}';"""
         c.execute(sql1)
         db.commit()
-   except Exception as e:
-     print(f"Error {e}")
-     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                         detail=f"{e}")    
+
 
 
 # @router.post('/transactions/{order_id}',response_model = schema.receipt)
@@ -103,11 +101,10 @@ async def create_transactions(new_transactions : schema.new_transactions,order_i
               '''
     c.execute(sqll,s)
     db.commit()
-    new_transactions.order_id=order_id[0],
+    new_transactions.order_id=order_id
     new_transactions.id_customer=str(current_user.id)
     new_transactions.order_date = datetime.now()
     new_transactions.total_prices=float(y[0][0])
-
     
    except Exception as e:
      print(f"Error {e}")
@@ -116,27 +113,28 @@ async def create_transactions(new_transactions : schema.new_transactions,order_i
    return new_transactions
 
 @router.delete('/delete-orders/{order_id}',status_code=status.HTTP_204_NO_CONTENT)
-async def delete_order(order_id : int,current_user : int = Depends(auth2.get_current_user)):
-   db = curso()
-   c = db.cursor()
-   try: 
-    sql = f"""select * from "transactions" where "orders_id" = '{order_id}' ; """
+async def delete_order(order_id : str,current_user : int = Depends(auth2.get_current_user)):
+    db = curso()
+    c = db.cursor()
+    sql = f"""select * from "transactions" where id_customer = '{int(current_user.id)}'
+                                          and"orders_id" = '{order_id}' 
+                                          and order_status = 'Processing' ; """
     c.execute(sql)
     x = c.fetchall()
+
     if len(x) == 0 :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"order with id: {order_id} does not exist")
-    if x[0][0] != int(current_user.id) :
+    if x[0][1] != int(current_user.id) :
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Not authorized to perform requested action")
     else:
-        sql1 = f"""delete from "transactions" where "orders_id" = '{order_id}' ;"""
+        sql1 = f"""delete from "transactions" where id_customer = '{int(current_user.id)}'
+                                          and"orders_id" = '{order_id}' 
+                                          and order_status = 'Processing';"""
         c.execute(sql1)
         db.commit()
-   except Exception as e:
-     print(f"Error {e}")
-     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                         detail=f"{e}")    
+  
     
     # if len(y) == 0:
     #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,

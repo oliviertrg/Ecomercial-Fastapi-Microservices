abe94e856d63d62c7e4b8dae5af51df44537fe33
oliviertrg <rioverrain@gmail.com>
2023-06-21 15:03:44 -0400

    update sending email for customer

--- git.sh
+++ git.sh
@@ -3,3 +3,5 @@ git add --all
 git commit -m "fix some history problem"
 
 git push -u origin main
+
+git push -f origin main
--- inventory_server/app/inventory_consumer.py
+++ inventory_server/app/inventory_consumer.py
@@ -1,6 +1,10 @@
 from kafka import KafkaConsumer
 import json
 from config import curso
+import smtplib, ssl
+from decouple import config
+
+
 
 ORDER_KAFKA_TOPIC = "cart_details"
 
@@ -16,6 +20,16 @@ consumer = KafkaConsumer(
 def kafka_event():
   print("sending ","="*200,">>>>>>>>>>>>>>>>>>>>>>")
   try: 
+
+    port = 465  # For SSL
+    smtp_server = config('gmail.com')
+    sender_email = config('gmail.com')  # Enter your address
+      # Enter receiver address
+    password = config("password")
+    message = """
+    Subject: Hi there
+
+    Thank you for shopping with us. If you want 15% off your next order, leave a review on our website."""
     db = curso()
     c = db.cursor()
     for i in consumer:
@@ -28,7 +42,14 @@ def kafka_event():
               """
        c.execute(sqll)
        db.commit()
+       
        print(f" confirm quantity of <{b['item_id']}> check was send ","="*50,">>>>>>")
+       receiver_email = b["email_customer"]
+       context = ssl.create_default_context()
+       with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        print(f"thank you email was sent to {receiver_email}")
   except Exception as e:
       print(f"Error {e}")  
 
